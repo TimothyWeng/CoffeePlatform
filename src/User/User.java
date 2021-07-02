3353b06c3cf8b729a83c2a77cd59b6ea9d0fe931
TimothyWeng <tim0309.tw@gmail.com>
2021-07-02 19:47:58 +0800

    delete unneed image

--- src/User/User.java
+++ src/User/User.java
@@ -333,7 +333,7 @@ public class User {
         Arrays.sort(array, new Comparator<int[]>() {
             @Override
             public int compare(int[] first, int[] second) {
-               if(first[columnNumber-1] > second[columnNumber-1]) return 1;
+               if(first[columnNumber-1] < second[columnNumber-1]) return 1;
                else return -1;
             }
         });
